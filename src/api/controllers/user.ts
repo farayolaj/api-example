b7959aa05f5099a5c2c812f5a8180ad5325d8dc0
Joshua Farayola <farayolajoshua@gmail.com>
2020-09-23 13:39:47 +0100

    logger middleware added

--- src/api/controllers/user.ts
+++ src/api/controllers/user.ts
@@ -1,7 +1,7 @@
 import * as express from 'express';
 
-import UserService from '../services/user';
-import { writeJsonResponse } from '../../utils/express';
+import UserService from '@apie/api/services/user';
+import { writeJsonResponse } from '@apie/utils/express';
 
 export function auth(req: express.Request, res: express.Response, next: express.NextFunction): void {
   const token = req.headers.authorization!;
--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,20 +1,33 @@
 import express from 'express';
-import {OpenApiValidator} from 'express-openapi-validator';
-import {Express} from 'express-serve-static-core';
-import {connector, summarise} from 'swagger-routes-express';
+import bodyParser from 'body-parser';
+import { OpenApiValidator } from 'express-openapi-validator';
+import { Express } from 'express-serve-static-core';
+import morgan from 'morgan';
+import morganBody from 'morgan-body';
+import { connector, summarise } from 'swagger-routes-express';
 import YAML from 'yamljs';
- 
-import * as api from '../api/controllers';
- 
+
+import * as api from '@apie/api/controllers';
+import logger from '@apie/utils/logger';
+import config from '@apie/config';
+import { expressDevLogger } from '@apie/utils/express_dev_logger';
+
 export async function createServer(): Promise<Express> {
   const yamlSpecFile = './config/openapi.yaml';
   const apiDefinition = YAML.load(yamlSpecFile);
   const apiSummary = summarise(apiDefinition);
-  console.info(apiSummary);
- 
+  logger.info(apiSummary);
+
   const server = express();
   // here we can intialize body/cookies parsers, connect logger, for example morgan
- 
+  server.use(bodyParser.json());
+  if (config.morganLogger)
+    server.use(morgan(':method :url :status :response-time ms - :res[content-length]'));
+  if (config.morganBodyLogger)
+    morganBody(server);
+  if (config.exmplDevLogger)
+    server.use(expressDevLogger);
+
   // setup API validator
   const validatorOptions = {
     coerceTypes: true,
@@ -23,7 +36,7 @@ export async function createServer(): Promise<Express> {
     validateResponses: true
   };
   await new OpenApiValidator(validatorOptions).install(server);
-  
+
   // error customization, if request is invalid
   server.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
     res.status(err.status).json({
@@ -34,11 +47,11 @@ export async function createServer(): Promise<Express> {
       }
     });
   });
- 
+
   const connect = connector(api, apiDefinition, {
     onCreateRoute: (method: string, descriptor: any[]) => {
       descriptor.shift();
-      console.log(`${method}: ${descriptor.map((d: any) => d.name).join(', ')}`);
+      logger.verbose(`${method}: ${descriptor.map((d: any) => d.name).join(', ')}`);
     },
     security: {
       bearerAuth: api.auth
@@ -46,6 +59,6 @@ export async function createServer(): Promise<Express> {
   });
 
   connect(server);
- 
+
   return server;
 }
